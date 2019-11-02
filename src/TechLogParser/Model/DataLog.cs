ad4955c54e212c3dfb4344bf89d59887ca80dec4
djserega <galyuk.sergey@gmail.com>
2019-11-02 21:57:43 +0200

    fix UI, added prop: ProcessName, ComputerName, User

--- src/TechLogParser/Model/DataLog.cs
+++ src/TechLogParser/Model/DataLog.cs
@@ -16,11 +16,34 @@ namespace TechLogParser.Model
                 null,
                 new System.Globalization.DateTimeStyles(),
                 out DateTime date);
+
             Date = date;
-            Text = text;
+            FullText = text;
+
+            ProcessName = FindParseText("p:processName");
+            ComputerName = FindParseText("t:computerName");
+            User = FindParseText("Usr");
+        }
+
+        private string FindParseText(string parameter)
+        {
+            string value = string.Empty;
+
+            int indexStartParameter = FullText.IndexOf(parameter);
+            if (indexStartParameter > 0)
+            {
+                indexStartParameter += parameter.Length + 1;
+                int indexEndParameter = FullText.IndexOf(",", indexStartParameter);
+                value = FullText.Substring(indexStartParameter, indexEndParameter - indexStartParameter);
+            }
+
+            return value;
         }
 
         public DateTime Date { get; set; }
-        public string Text { get; set; }
+        public string FullText { get; set; }
+        public string ProcessName { get; set; }
+        public string ComputerName { get; set; }
+        public string User { get; set; }
     }
 }
